156a79943601efd57400345cb3c524ff8dce82c3
Nuno Figueiredo <nunosilvares@hotmail.com>
2022-02-10 16:57:22 +0000

    Common scripts from previous repository

---
+++ GameObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Madelinette.Common
+{
+    public abstract class GameObject
+    {
+        public int[] Position { get; set; }
+
+        public abstract char Sprite { get; }
+
+        public abstract void Update();
+    }
+}
